a9531eef8940b778104911719d51d50a7d3cf2e6
David Julian Spiess <spiessapps@gmail.com>
2021-06-03 16:45:58 -0300

    adding second part

--- challenge.js
+++ challenge.js
@@ -1,4 +1,6 @@
-/*const challenge = function(list){
+/*
+First approach;
+const challenge = function(list){
     let amount = 0;
     let resList = [];
     for(let i = 0; i < list.length; i++){
@@ -16,9 +18,10 @@
 
 
 const challenge = function(list){
+    //Moving 0s:
     let amount = 0;
     const resList = list;
-    const n = list.length;
+    let n = list.length;
 
     for(let i = 0; i < n; i++){
         if(resList[i] !== 0){
@@ -28,7 +31,63 @@ const challenge = function(list){
     while(amount < n){
         resList[amount++] = 0;
     }
-   return resList;
+   console.log(resList);
+
+   for(let i = (n-1); i >= 0; i--){
+        if(resList[i] === 0){
+            resList.pop();
+        }else{
+            break;
+        }
+   }
+
+   console.log(resList);
+
+
+   //adding 1 unit:
+    n = resList.length;
+    resList[n-1] += 1;
+    let carry = resList[n-1]/10;
+    resList[n-1] = resList[n-1] % 10;
+
+    for(let i = n - 2; i >= 0; i--){
+        if(carry === 1){
+            resList[i] += 1;
+            carry = resList[i]/10;
+            resList[i] = resList[i] % 10;
+        }
+    }
+
+    if(carry === 1) resList.unshift(1)
+
+
+    console.log(resList);
+
+    //Multiplying -1 in evens
+    n = resList.length;
+
+    for(let i = 0; i < n; i++){
+        if( i % 2 === 0){
+            resList[i] = resList[i] * -1;
+        }
+    }
+
+    console.log(resList);
+
+
+    //Max sum of subarray
+    let maxSum = -Infinity;
+    let currSum = 0;
+
+    for(let i = 0; i < resList.length; i++){
+        currSum = Math.max(resList[i], currSum + resList[i]);
+        maxSum = Math.max(currSum, maxSum);
+    }
+
+    console.log(maxSum);
+
 }
 
-console.log(challenge([4,8,0,9,2,5,0,3,3,0]));
+//challenge([4,8,0,9,2,5,0,3,3,0]);
+
+challenge([4,8,9,2,5,8,4,8,0]);
